27a542bbb3ff686f62f768c876b5ed668f9adf4c
Jan Ullmann <jan.ullmann@student.uni-tuebingen.de>
2023-11-21 14:06:15 +0100

    added tasks

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from semmy import app
 
 if __name__ == '__main__':
-    app.run()
-    # app.run(img_path=r"img/2023-06-29-D5-11-01.tif")
+    # app.run()
+    app.run(img_path=r"img/2023-06-29-D5-11-01.tif")
+
+# TODO
+# Output
+# - implement selection of object in vispy canvas
+# - proper update of the shown elements and properties
+#
+# Settings
+# - proper output of settings file
+# - settings page
