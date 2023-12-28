c2689f455e57f2275fd7b52f0bada43196911452
Jan Ullmann <102742052+ullmannJan@users.noreply.github.com>
2023-12-29 00:38:17 +0100

    versioning update again

--- src/semmy/__init__.py
+++ src/semmy/__init__.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from importlib.metadata import version, PackageNotFoundError
 
 try:
     from ._version import version as __version__
@@ -7,6 +6,5 @@ try:
 except ImportError:
     __version__ = "unknown version"
     version_tuple = (0, 0, "unknown version")
-
 
 semmy_path = Path(__file__).parent.resolve()
