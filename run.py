c953b5848618fd21f3dcdcfe1e1e026ab1d7e0a1
Jan Ullmann <jan.ullmann@student.uni-tuebingen.de>
2024-01-18 12:32:56 +0100

    logging level changed

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import semmy
 import logging
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.CRITICAL)
     # semmy.run()
     semmy.run(file_path=r"tests/test_data/test_file.semmy")
     # semmy.run(file_path=r"tests/test_data/test_image.tif")
