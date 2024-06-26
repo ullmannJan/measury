bb7cbc750b79098dc45f55902766884c2080bc06
Jan Ullmann <102742052+ullmannJan@users.noreply.github.com>
2024-06-26 05:32:11 +0200

    updated compiling workflow

--- compiling/compile.py
+++ compiling/compile.py
@@ -1,15 +1,17 @@
 import PyInstaller.__main__
 import shutil
+import sys
 from pathlib import Path
 from measury import __version__ as measury_version
 script_path = Path(__file__).parent.resolve()
-
 
 if __name__ == '__main__':
     # set program name and method
     program_name = "Measury" + "_" + measury_version
     method = "onedir"
-    zipped = False
+    zipped = True
+    if len(sys.argv) > 1 and sys.argv[1] == "zipped":
+        zipped = True
 
     # run pyinstaller
     PyInstaller.__main__.run([
@@ -26,6 +28,6 @@ if __name__ == '__main__':
     ])
 
     # zip output for easy distribution
-    output_path = (script_path/f"/../dist/{program_name}").resolve()
+    output_path = (script_path/f"../dist/{program_name}").resolve()
     if zipped:
         shutil.make_archive(output_path, 'zip', output_path)
